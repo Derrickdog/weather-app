c027af81d139a8c04965a2cb75702e6b98e7fd92
Derrick <derrickryan10@gmail.com>
2022-06-24 15:22:22 -0400

    Add fetcher

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ const tempHighText = document.getElementById('temp-high');
 const tempLowText = document.getElementById('temp-low');
 const weatherText = document.getElementById('weather');
 
-fetchWeather();
+const searchField = document.getElementById('search');
+const searchButton = document.getElementById('search-btn');
 
-async function fetchWeather() {
-    let city = 'london';
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=67bcb51d44ba92aa7e2060b44f068eb6&q`, {mode: 'cors'});
-    const weatherData = await response.json();
-    // console.log(weatherData);
-    // console.log(weatherData.name);
-    // console.log(weatherData.main.temp);
-    // console.log(weatherData.main.temp_min);
-    // console.log(weatherData.main.temp_max);
-    // console.log(weatherData.weather[0].main);
-    displayText(weatherData.name, weatherData.main.temp, weatherData.main.temp_max, weatherData.main.temp_min, weatherData.weather[0].main);
+searchButton.addEventListener('click', () => {
+    // fetchWeather(searchField.value);
+});
+
+// fetchWeather('chicago');
+// if (!response.ok) throw new Error(`City ${city} not found`);
+// const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=67bcb51d44ba92aa7e2060b44f068eb6&q`, {mode: 'cors'});
+
+fetchWeather(41.061150, -74.013000);
+// fetchWeather(-37.813629, 144.963058);
+
+async function fetchWeather(lat, lon) {
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=67bcb51d44ba92aa7e2060b44f068eb6&q`, {mode: 'cors'});
+        const weatherData = await response.json();
+        displayText(weatherData.name, weatherData.main.temp, weatherData.main.temp_max, weatherData.main.temp_min, weatherData.weather[0].main);
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 
 function displayText(city, temp, tempHigh, tempLow, weather) {
