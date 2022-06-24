00a698b874efd6593a9ba630b357b7566a7ca907
Derrick <derrickryan10@gmail.com>
2022-06-24 19:04:11 -0400

    Update location info

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const searchButton = document.getElementById('search-btn');
 const errorText = document.getElementById('error');
 
 const cityText = document.getElementById('city');
+
 const tempText = document.getElementById('temp');
 const tempHighText = document.getElementById('temp-high');
 const tempLowText = document.getElementById('temp-low');
@@ -19,6 +20,7 @@ async function fetchCity(city) {
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`, {mode: 'cors'});
         const cityData = await response.json();
         fetchWeather(cityData[0].lat, cityData[0].lon);
+        displayCity(cityData);
     }
     catch(err) {
         displayError();
@@ -29,19 +31,31 @@ async function fetchWeather(lat, lon) {
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`, {mode: 'cors'});
         const weatherData = await response.json();
-        displayText(weatherData.name, weatherData.main.temp, weatherData.main.temp_max, weatherData.main.temp_min, weatherData.weather[0].main);
+        displayText(weatherData);
     }
     catch(err){
         console.log(err);
     }
 }
 
-function displayText(city, temp, tempHigh, tempLow, weather) {
-    cityText.textContent = city;
-    tempText.textContent = temp;
-    tempHighText.textContent = tempHigh;
-    tempLowText.textContent = tempLow;
-    weatherText.textContent = weather;
+function displayCity(cityData) {
+    const currentCountry = cityData[0].country;
+    let location = '';
+
+    location += cityData[0].name;
+    if(currentCountry === 'US' || currentCountry === 'GB'){
+        location += ', ' + cityData[0].state;
+    }
+    location += ', ' + currentCountry;
+
+    cityText.textContent = location;
+}
+
+function displayText(weatherData) {
+    tempText.textContent = weatherData.main.temp;
+    tempHighText.textContent = weatherData.main.temp_max;
+    tempLowText.textContent = weatherData.main.temp_min;
+    weatherText.textContent = weatherData.weather[0].main;
 }
 
 function displayError(){
